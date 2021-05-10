abba11b2ce6bce24c48a4bc63d1c8700732e2e96
Whiplash <kountdown@mail.ru>
2021-05-10 12:33:18 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/14_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14_6.exe"
+  "14_6.exe.manifest"
+  "14_6.pdb"
+  "CMakeFiles/14_6.dir/14_6_4_Comparison.cpp.obj"
+  "lib14_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
