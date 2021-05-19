c9db59bf146590454039f7008f4642404770c328
Whiplash <kountdown@mail.ru>
2021-05-19 21:06:06 +0300

    Done

--- 14_6_5_TicTacToe.cpp
+++ 14_6_5_TicTacToe.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 bool correctInput(string str) {
-    for (int i = 0; i < 3; i++) {
-        if (str[i] != '.' && str[i] != 'O' && str[i] != 'X') {
-            return false;
+    if (str.length() != 3) return false;
+    else {
+        for (int i = 0; i < 3; i++) {
+            if (str[i] != '.' && str[i] != 'O' && str[i] != 'X') {
+                return false;
+            }
         }
     }
     return true;
@@ -13,7 +16,7 @@ bool correctInput(string str) {
 
 
 int checkingRows(string str[], char mark) {
-    bool win = false;
+    bool win;
     int winCounter = 0;
     for (int i = 0; i < 3; i++) {
         win = true;
@@ -27,12 +30,11 @@ int checkingRows(string str[], char mark) {
             winCounter++;
         }
     }
-
     return winCounter;
 }
 
 int checkingCols(string str[], char mark) {
-    bool win = true;
+    bool win;
     int winCounter = 0;
     for (int i = 0; i < 3; i++) {
         win = true;
@@ -50,12 +52,9 @@ int checkingCols(string str[], char mark) {
 }
 
 int checkingDiagonals(string str[], char mark) {
-    bool win = true;
     int winCounter = 0;
 
-
     if (str[1][1] == mark) {
-
         if (str[0][0] == mark && str[2][2] == mark) {
             winCounter++;
         }
@@ -66,23 +65,28 @@ int checkingDiagonals(string str[], char mark) {
     return winCounter;
 }
 
-bool gameWon(string str[], char mark) {
-    return checkingRows(str, mark) == 1 || checkingCols(str, mark) == 1 || checkingDiagonals(str, mark) == 1;
+int gameWon(string str[], char mark) {
+    return checkingRows(str, mark) + checkingCols(str, mark) + checkingDiagonals(str, mark);
 }
 
 string gameResult(string str[]) {
-    int x = 0, y = 0;
-    int circle = 0, cross = 0;
+    int circles = 0, crosses = 0;
+    int circleWinCounter = gameWon(str, 'O');
+    int crossWinCounter = gameWon(str, 'X');
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (str[i][j] == 'X') cross++;
-            else if (str[i][j] == 'O') circle++;
+            if (str[i][j] == 'X') crosses++;
+            else if (str[i][j] == 'O') circles++;
         }
     }
-    if ( (gameWon(str, 'O') && gameWon(str, 'X'))
-    || (gameWon(str, 'O') && (cross > circle || circle > cross)) || (gameWon(str, 'X')
-    && (circle >= cross || cross - circle >=  2)) || (gameWon(str, 'O') > 1)
-    || (gameWon(str, 'X') > 1) ) {
+
+    // ()
+    int diff = crosses - circles;
+    if ((circleWinCounter == 1 && crossWinCounter == 1)
+        || (circleWinCounter == 1 && (crosses != circles))
+        || (crossWinCounter == 1 && (diff != 1))
+        || (circleWinCounter > 1) || (crossWinCounter > 1)
+        || (crossWinCounter + circleWinCounter == 0 && (diff != 0 && diff != 1))) {
         return "Incorrect";
     } else if (gameWon(str, 'O')) {
         return "Vanya won";
